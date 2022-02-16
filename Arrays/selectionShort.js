5419f62d3d0eb6c43319257e72459e82cf57579e
iferhatzdemir <ferhatzdemir9@gmail.com>
2022-02-17 02:02:04 +0300

    comment satırı eklendi

--- Arrays/selectionShort.js
+++ Arrays/selectionShort.js
@@ -2,8 +2,8 @@ let array = [1,23,45,3,555,63];
 
 for (let index = 0; index < array.length; index++) {
    let enkucukID =index;
-   for (let j = index+1; j < array.length; j++) {
-       if(array[j]<array[enkucukID]){
+   for (let j = index+1; j < array.length; j++) { 
+       if(array[j]<array[enkucukID]){ // eğer buradaki büyüktür küçüktür işaretini değiştirerek arrayi büyükten küçüğe veye küçükten büyüyüğe sıralayabiliriz.
         enkucukID=j;
        }
        
