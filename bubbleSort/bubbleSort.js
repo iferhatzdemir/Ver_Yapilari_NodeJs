2c7a086e49a5cbd9b8935026e31f21a6fe4ef49c
iferhatzdemir <ferhatzdemir9@gmail.com>
2022-02-17 19:35:10 +0300

    eklemeler yapıldı

---
+++ bubbleSort/bubbleSort.js
@@ -0,0 +1,9 @@
+let array=[90,959,8548,93,34,875,8,6,43,2,4,5,6,54,64674,3,54,42,3,432]
+
+for (let index = 0; index < array.length; index++) {
+    for (let j = 0; j < array.length; j++) {
+        const element = array[index];
+        
+    }
+    
+}
--- selectionSort/selectionShort.js
+++ selectionSort/selectionShort.js
@@ -1,4 +1,4 @@
-let array = [1,23,45,3,555,63];
+/*let array = [1,23,45,3,555,63];
 
 for (let index = 0; index < array.length; index++) {
    let enkucukID =index;
@@ -15,4 +15,23 @@ for (let index = 0; index < array.length; index++) {
     array[enkucukID] = tmp;      
 }
 }
-console.log(array);
+console.log(array);*/
+function insertionSort(inputArr) {
+    let n = inputArr.length;
+        for (let i = 1; i < n; i++) {
+            // Choosing the first element in our unsorted subarray
+            let current = inputArr[i];
+            // The last element of our sorted subarray
+            let j = i-1; 
+            while ((j > -1) && (current < inputArr[j])) {
+                inputArr[j+1] = inputArr[j];
+                j--;
+                console.log(inputArr)
+            }
+            inputArr[j+1] = current;
+        }
+    return inputArr;
+};
+let inputArr = [5,21,35,28,15,12,22];
+insertionSort(inputArr);
+console.log(inputArr);
