3bf4db7469d73d4281eebc610fe57427f776e87d
iferhatzdemir <ferhatzdemir9@gmail.com>
2022-02-23 22:10:38 +0300

    adımlar için console.log eklendi

--- mergeSort/mergeSort.js
+++ mergeSort/mergeSort.js
@@ -1,17 +1,17 @@
 function merge(left, right) {
     let arr = []
-    // Break out of loop if any one of the array gets empty
     while (left.length && right.length) {
-        // Pick the smaller among the smallest element of left and right sub arrays 
         if (left[0] < right[0]) {
-            arr.push(left.shift())  
+            arr.push(left.shift());
+            console.log("Left :"+left);
         } else {
             arr.push(right.shift()) 
+            console.log("Right"+right)
         }
+        console.log("Array :"+arr);
     }
     
-    // Concatenating the leftover elements
-    // (in case we didn't go through the entire left or right array)
+    
     return [ ...arr, ...left, ...right ]
 }
 
